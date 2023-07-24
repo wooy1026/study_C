3abe98a853b4eec86ac2bc991daed6003ba34b89
choiwooyoung <wooy1026@naver.com>
2023-07-24 11:48:19 +0900

    10807

---
+++ study_project/10807.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int a[100];
+	int v;
+	int count = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	scanf("%d", &v);
+
+	for (int j = 0; j < n; j++)
+	{
+		if (a[j] == v)
+			count++;
+	}
+
+	printf("%d", count);
+
+	return 0;
+}
